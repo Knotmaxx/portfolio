ba50ce26c898c396342b9fa4a69778aca6b0908e
Knotmaxx <diesknot@gmail.com>
2023-06-06 15:59:37 -0300

    commit all

--- script/skills.js
+++ script/skills.js
@@ -9,7 +9,7 @@
 
 
 const skillDados = [
-  [{name:"DEVELOPER",description:"Tenho habilidade com programação para entregar interatividade para o usuário, botões, integração com google maps, menu, responsividade e etc.", src:"../assets/habilidades/developer.svg"}]
+  [{name:"DEVELOPER",description:"Tenho habilidade com programação para entregar interatividade em seu site como botões, integração com google maps, menus, formulários e etc.", src:"../assets/habilidades/developer.svg"}]
 ,[{name:"DESIGN",description:"Minha habilidade com design combina a identidade visual da sua marca de forma intuitiva e moderna para gerar uma boa experiência para seus usuários através do design.", src:"../assets/habilidades/design.svg"}],
 [{name:"SEO",description:"Além de ter um bom desenvolvimento, um site  deve ter também um bom SEO para ser no mínimo um dos primeiros nos mecanismos de busca e ter uma boa visibilidade.", src:"../assets/habilidades/seo.svg"}],
 [{name: "OTIMIZAÇÃO", description:"Consigo entregar um site bem otimizado para carregar no máximo em 2s, sendo este o tempo máximo até o usuário sair do site por falta de paciência!", src:"../assets/habilidades/otimizar.svg"}]
