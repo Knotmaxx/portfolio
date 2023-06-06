958e51006dda2cdfe3744b49faee1c9e61a881c3
Knotmaxx <diesknot@gmail.com>
2023-06-06 12:22:23 -0300

    commit all

--- script/skills.js
+++ script/skills.js
@@ -1,9 +1,20 @@
-const skillDados = [[{name:"HTML",description:"HTML é uma linguagem de marcação de hipertexto, sendo o pilar de um site, podendo adicionar texto, imagens, videos etc. De forma estruturada e organizada.", src:"https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"}]
-,[{name:"CSS",description:"Cascading Style Sheets ou CSS, é a folha de estilo do site, possibilitando adicionar estilos no html como animações, cores, responsividade(site para celulares,tablets) etc.", src:"https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"}],
-[{name:"JAVASCRIPT",description:"JavaScript é uma linguagem de programação com foco principal no front-end(visual do site), utilizada para dar interatividade com o usuário, como formularios, cookies, elementos dinâmicos(botões) etc.", src:"https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"}],
-[{name: "BOOTSTRAP", description:"BootStrap é um framework web com o intuito de otimizar o tempo com a criação do estilo do site de forma responsiva. ", src:"https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg"}],
-[{name: "PHP", description:"PHP é uma linguagem de programação do lado do servidor, amplamente utilizada na criação de sites dinâmicos, conectados a bancos de dados e Apis. Inclusive o Wordpress é feito em php!", src:"https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg"}]
+// const skillDados = [[{name:"HTML",description:"HTML é uma linguagem de marcação de hipertexto, sendo o pilar de um site, podendo adicionar texto, imagens, videos etc. De forma estruturada e organizada.", src:"https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"}]
+// ,[{name:"CSS",description:"Cascading Style Sheets ou CSS, é a folha de estilo do site, possibilitando adicionar estilos no html como animações, cores, responsividade(site para celulares,tablets) etc.", src:"https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"}],
+// [{name:"JAVASCRIPT",description:"JavaScript é uma linguagem de programação com foco principal no front-end(visual do site), utilizada para dar interatividade com o usuário, como formularios, cookies, elementos dinâmicos(botões) etc.", src:"https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"}],
+// [{name: "BOOTSTRAP", description:"BootStrap é um framework web com o intuito de otimizar o tempo com a criação do estilo do site de forma responsiva. ", src:"https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg"}],
+// [{name: "PHP", description:"PHP é uma linguagem de programação do lado do servidor, amplamente utilizada na criação de sites dinâmicos, conectados a bancos de dados e Apis. Inclusive o Wordpress é feito em php!", src:"https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg"}]
+// ]
+
+
+
+
+const skillDados = [
+  [{name:"DEVELOPER",description:"Tenho habilidade com programação para entregar interatividade para o usuário, botões, integração com google maps, menu, responsividade e etc.", src:"../assets/habilidades/developer.svg"}]
+,[{name:"DESIGN",description:"Minha habilidade com design combina a identidade visual da sua marca de forma intuitiva e moderna para gerar uma boa experiência para seus usuários através do design.", src:"../assets/habilidades/design.svg"}],
+[{name:"SEO",description:"Além de ter um bom desenvolvimento, um site  deve ter também um bom SEO para ser no mínimo um dos primeiros nos mecanismos de busca e ter uma boa visibilidade.", src:"../assets/habilidades/seo.svg"}],
+[{name: "OTIMIZAÇÃO", description:"Consigo entregar um site bem otimizado para carregar no máximo em 2s, sendo este o tempo máximo até o usuário sair do site por falta de paciência!", src:"../assets/habilidades/otimizar.svg"}]
 ]
+
 
 
 export {skillDados}
