73e1da573671eb653eeccfb65954309abd839e65
Knotmaxx <100964850+Knotmaxx@users.noreply.github.com>
2023-06-20 10:59:30 -0300

    Update skills.js

--- script/skills.js
+++ script/skills.js
@@ -1,9 +1,3 @@
-// const skillDados = [[{name:"HTML",description:"HTML é uma linguagem de marcação de hipertexto, sendo o pilar de um site, podendo adicionar texto, imagens, videos etc. De forma estruturada e organizada.", src:"https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"}]
-// ,[{name:"CSS",description:"Cascading Style Sheets ou CSS, é a folha de estilo do site, possibilitando adicionar estilos no html como animações, cores, responsividade(site para celulares,tablets) etc.", src:"https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"}],
-// [{name:"JAVASCRIPT",description:"JavaScript é uma linguagem de programação com foco principal no front-end(visual do site), utilizada para dar interatividade com o usuário, como formularios, cookies, elementos dinâmicos(botões) etc.", src:"https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"}],
-// [{name: "BOOTSTRAP", description:"BootStrap é um framework web com o intuito de otimizar o tempo com a criação do estilo do site de forma responsiva. ", src:"https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg"}],
-// [{name: "PHP", description:"PHP é uma linguagem de programação do lado do servidor, amplamente utilizada na criação de sites dinâmicos, conectados a bancos de dados e Apis. Inclusive o Wordpress é feito em php!", src:"https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg"}]
-// ]
 
 
 
